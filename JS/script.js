98450195e7399c175a63064cf1888b9e5c8a3f48
Jose Ramirez <jsayram@Gmail.com>
2017-06-10 11:36:37 -0400

    Re-stiched the URL for the images and appended the appropriate width for the images in a loop

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,22 @@
 // so first thing we need to create an array that holds all images on the page
 const IMAGES = document.querySelectorAll("img");
 
+
+// because theres several image sizes, this will create the corrrect URL for the images
+function makeSrcset(imgSrc) {
+    let markup = [];
+    let width = 400; // thats the smallest size for the images , and they increment by 400
+
+    for (let i = 0; i < 5; i++) { //5 times for 5 different image sizes
+
+        //imgSrc is the URL without the -###.jpg at the end
+        markup[i] = imgSrc + "-" + width + ".jpg " + width + "w"; // this is what it should look like
+        width += 400;
+    }
+
+    return markup.join(); //to get a comma seperated list
+}
+
 //create a loop, that loops through each of the images
 for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
@@ -8,7 +24,8 @@ for (let i = 0; i < IMAGES.length; i++) {
      this takes off the -###.jpg at the end of each image , so that later we can put the right size for the image*/
     imgSrc = imgSrc.slice(0, -8);
 
-    console.log(imgSrc);
+    let srcset = makeSrcset(imgSrc);
+    console.log(srcset);
 
     //in addition to the source atrribute we need an addition source for the image , and that is the datatype
     //--(note this attribute was custom created)
